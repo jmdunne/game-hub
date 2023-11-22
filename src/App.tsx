fdb74e26b640e9c28ebdfa26fb907586c682a493
Josh Dunne <joshua.m.dunne@gmail.com>
2023-11-22 15:08:54 -0700

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
+// Importing the necessary components from Chakra UI and the NavBar component.
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar"; // Import NavBar from the components folder.
 
 function App() {
   return (
+    // Grid component to create a flexible layout with named areas.
     <Grid
       templateAreas={{
-        base: '"nav" "main"',
-        lg: '"nav nav" "aside main"',
+        base: '"nav" "main"', // Single column layout for small screens.
+        lg: '"nav nav" "aside main"', // Two-row layout for large screens.
       }}
     >
+      {/* GridItem for the navigation area */}
       <GridItem area="nav">
-        <NavBar />
+        <NavBar />{" "}
+        {/* NavBar component included inside the navigation GridItem */}
       </GridItem>
+
+      {/* Conditionally rendered GridItem for the aside area on large screens */}
       <Show above="lg">
         <GridItem area="aside" bg="gold">
           Aside
         </GridItem>
       </Show>
+
+      {/* GridItem for the main content area with a dodgerblue background */}
       <GridItem area="main" bg="dodgerblue">
         Main
       </GridItem>
     </Grid>
   );
 }
-export default App;
+export default App; // Exporting the App function as a default export.
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,18 @@
+// Importing Chakra UI components for building the NavBar.
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/logo.webp"; // Importing the logo image.
 
 const NavBar = () => {
   return (
+    // HStack (Horizontal Stack) component to align items horizontally.
     <HStack>
+      {/* Image component displaying the logo, with a specified box size */}
       <Image src={logo} boxSize="60px" />
+
+      {/* Text component to display the label 'NavBar' */}
       <Text>NavBar</Text>
     </HStack>
   );
 };
 
-export default NavBar;
+export default NavBar; // Exporting the NavBar as a default export for use in other files.
